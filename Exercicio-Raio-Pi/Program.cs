fe95e808de6f71c08e90c409d408516da791e152
Geremias Santos <Geremias99999@gmail.com>
2022-06-13 09:09:38 -0300

    Exercise Raio/Pi

---
+++ Exercicio-Raio-Pi/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio_Raio_Pi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Programa para mostrar a área de um circulo!
+
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double pi = 3.14159;
+
+            double total = pi * raio * raio;
+
+            Console.WriteLine("Resultado " + total.ToString("F4", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
